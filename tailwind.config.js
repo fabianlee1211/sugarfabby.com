c357874000ad25b3321d334c212e37fb7924acd5
Fabian Lee <chleefabian@gmail.com>
2021-01-30 04:31:07 +0800

    fix: font family (#355)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'system-ui'],
+        sans: [
+          'Inter',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Oxygen',
+          'Ubuntu',
+          'Cantarell',
+          'Open Sans',
+          'Helvetica Neue',
+          'sans-serif',
+        ],
       },
       colors: {
         primary: {
